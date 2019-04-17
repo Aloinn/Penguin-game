ce57376254dc26b290c0b28fe5ddce2b5d1034f6
Aloin <onealainn@gmail.com>
2019-04-16 17:36:47 -0700

    Added JOIN ROOM text input

--- static/game.js
+++ static/game.js
@@ -6,5 +6,27 @@ socket.on('message', function(data) {
   console.log(data);
 });
 
+// CANVAS SETUP
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ratioX = 0;
+var ratioY = 0;
+var ratio = 0;
+function canvasetup(){
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  ratioX = canvas.width/800;
+  ratioY = canvas.height/600;
+  ratio  = Math.min(ratioX,ratioY);
+  ratio = Math.min(ratio,1.2)
+}
+window.addEventListener('resize',canvasetup);
+canvasetup();
+
+// CLEAR FIELD
+function clearfield(){
+  this.value = "";
+}
+
 // ON CONNECT
 socket.emit('create room');
